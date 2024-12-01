53d2896f44233e5bf736e39046828e746c9369ca
kalatasql <kalata.sql@gmail.com>
2024-12-01 18:29:52 +0200

    Update Three_Ways_To_Return_Invoices_Without_InvoicesDetails.sql

--- Three_Ways_To_Return_Invoices_Without_InvoicesDetails.sql
+++ Three_Ways_To_Return_Invoices_Without_InvoicesDetails.sql
@@ -1,28 +1,26 @@
-	SELECT inv.* -- Show all columns 
-	FROM invoices inv -- From table invoices
-	LEFT JOIN InvoicesDetails invD ON inv.ID = invD.invoice_Id -- Join the invoices with InvoicesDetails based on invoice_ID
-	-- The LEFT JOIN ensures all records from invoices are included, even if there is no matching record in InvoicesDetails
-	WHERE invD.invoice_Id IS NULL; -- This means there is no matching Invoice_ID in the InvoicesDetails for the current Invoice_ID in the Invoices table.
 
+1. WITH LEFT JOIN
+SELECT inv.* -- Show all columns 
+FROM invoices inv -- From table invoices
+LEFT JOIN InvoicesDetails invD ON inv.ID = invD.invoice_Id -- Join the invoices with InvoicesDetails based on invoice_ID
+-- The LEFT JOIN ensures all records from invoices are included, even if there is no matching record in InvoicesDetails
+WHERE invD.invoice_Id IS NULL; -- This means there is no matching Invoice_ID in the InvoicesDetails for the current Invoice_ID in the Invoices table.
 
-  SELECT *
-  FROM
-  invoices inv
-  WHERE NOT EXISTS
-  ( 
-  SELECT 
-	  1
-  FROM InvoicesDetails invD 
-  WHERE invD.Invoice_ID = inv.ID 
-  )
+2. With SubQuery and NOT EXISTS in WHERE Clause 
+SELECT *
+FROM invoices inv
+WHERE NOT EXISTS ( 
+  		  SELECT 1
+  		  FROM InvoicesDetails invD 
+  		  WHERE invD.Invoice_ID = inv.ID 
+ 		 )
 
-  SELECT *
-  FROM
-  invoices inv
-  WHERE id NOT IN
-  ( 
-    SELECT 
-	  invD.invoice_Id
-  FROM InvoicesDetails invD 
-  WHERE invD.Invoice_ID = inv.ID 
-)
+3. WITH SubQuery and NOT IN in WHERE Clause
+SELECT *
+FROM invoices inv
+WHERE id NOT IN ( 
+    		 SELECT 
+	 	 invD.invoice_Id
+FROM InvoicesDetails invD 
+WHERE invD.Invoice_ID = inv.ID 
+		)
